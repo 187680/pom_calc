4dd47c27d104eb2cc750d11efc2cd1356c6c7bd3
Bartosz Dziubałtowski <b.dziubaltowski@ba-t430-03.office.binarapps.com>
2018-11-03 15:13:43 +0100

    test branch

---
+++ app/src/main/java/com/example/king/pam_calculator/ExtendedActivity.java
@@ -0,0 +1,13 @@
+package com.example.king.pam_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExtendedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extended);
+    }
+}
